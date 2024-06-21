3c6beb4031884ae0f007e14a3daadddcbd99da1e
Mustafa Stietieh <steitieh03@gmail.com>
2024-06-21 18:57:45 +0300

    Added age to code Person1

--- WelcomeMessage.java
+++ WelcomeMessage.java
@@ -5,7 +5,10 @@ public class WelcomeMessage {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name please :) : ");
         String name = scanner.nextLine();
+        System.out.print("Enter your age please: ");
+        String age = scanner.nextLine();
         System.out.println("Welcome, " + name + "! \n Hope you have a great day!!");
+        System.out.println("You are " + age " years old!!");
         scanner.close();
     }
 }
